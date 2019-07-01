dee09ae173c202e1ece922cde0dbc974625e7bb0
Kevin Wang <rocwang0316@gmail.com>
2019-07-01 16:48:51 -0700

    Added the Posts reducer to the index

--- app/store/Index.ts
+++ app/store/Index.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 // import { routerReducer } from 'react-router-redux';
 
+import posts from './Posts/reducers';
+
 export default combineReducers({
-  
+  posts,
 });
