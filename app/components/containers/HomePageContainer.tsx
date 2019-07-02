55d843d61f56662313c9c4cf5b603654dce636f9
Kevin Wang <rocwang0316@gmail.com>
2019-07-01 19:08:01 -0700

    Moved the redux states and fetching action to the child component.

--- app/components/containers/HomePageContainer.tsx
+++ app/components/containers/HomePageContainer.tsx
@@ -1,56 +1,28 @@
-import React, { useEffect, memo } from 'react';
-import { connect } from 'react-redux';
+import React, { memo } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core';
+import { Typography, Paper } from '@material-ui/core';
+import I18n from '@kevinwang0316/i18n';
 
-import { AppState } from '../../store/ConfigureStore';
-import { PostsType } from '../../store/Posts/types';
-import { PostCountType } from '../../store/PostCount/types';
-import { fetchPosts as fetchPostsAction } from '../../store/Posts/actions';
-import { fetchPostCount as fetchPostCountAction } from '../../store/PostCount/actions';
-import { MAX_POSTS_AMOUNT } from '../../config';
-
-interface Props {
-  posts: PostsType;
-  postCount: PostCountType;
-  fetchPosts: (offset: number) => void;
-  fetchPostCount: Function;
-}
+import PostList from './PostList';
 
 const useStyles = makeStyles({
-  cardContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    margin: '20px 0 30px 0',
-    flexWrap: 'wrap',
-    '& > div': {
-      marginBottom: 10,
-    },
-  },
-  rootDiv: {
-    paddingLeft: 50,
+  rootPaper: {
+    margin: '50px auto 10px auto',
+    width: '80%',
+    padding: 15,
   },
 });
 
-const HomePage = ({
-  posts = null, postCount = null, fetchPosts, fetchPostCount,
-}: Props) => {
+const HomePage = () => {
   const classes = useStyles({});
-  useEffect(() => {
-    if (!posts) fetchPosts(0);
-    if (!postCount) fetchPostCount();
-  });
-
   return (
-    <div className={classes.rootDiv}>
-      aaa
-    </div>
-
+    <Paper className={classes.rootPaper}>
+      <Typography variant="h5" component="h4">
+        {I18n.get('postListTitle')}
+      </Typography>
+      <PostList />
+    </Paper>
   );
 };
 
-/* istanbul ignore next */
-const mapStateToProps = ({ posts, postCount }: AppState) => ({ posts, postCount });
-/* istanbul ignore next */
-const mapDispatchToProps = { fetchPostCount: fetchPostCountAction, fetchPosts: fetchPostsAction };
-export default connect(mapStateToProps, mapDispatchToProps)(memo(HomePage));
+export default memo(HomePage);
