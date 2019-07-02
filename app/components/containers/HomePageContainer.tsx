c01f5780270cd4e844e32275e43ea73cb8697233
Kevin Wang <rocwang0316@gmail.com>
2019-07-01 19:55:27 -0700

    Updated the container component to use new components.

--- app/components/containers/HomePageContainer.tsx
+++ app/components/containers/HomePageContainer.tsx
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Paper } from '@material-ui/core';
 import I18n from '@kevinwang0316/i18n';
 
-import PostList from './PostList';
+import PostList from '../PostList';
 
 const useStyles = makeStyles({
   rootPaper: {
@@ -17,9 +17,6 @@ const HomePage = () => {
   const classes = useStyles({});
   return (
     <Paper className={classes.rootPaper}>
-      <Typography variant="h5" component="h4">
-        {I18n.get('postListTitle')}
-      </Typography>
       <PostList />
     </Paper>
   );
