17e0850d0e901f88251c602fb068b9afa532dec2
Kevin Wang <rocwang0316@gmail.com>
2019-07-01 17:12:00 -0700

    Added the PostCount reducer to the root reducer.

--- app/store/Index.ts
+++ app/store/Index.ts
@@ -2,7 +2,9 @@ import { combineReducers } from 'redux';
 // import { routerReducer } from 'react-router-redux';
 
 import posts from './Posts/reducers';
+import postCount from './PostCount/reducers';
 
 export default combineReducers({
+  postCount,
   posts,
 });
