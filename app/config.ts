3f9e458a38f30362027b8b28d7ce3f8169ba4106
Kevin Wang <rocwang0316@gmail.com>
2019-07-02 10:14:01 -0700

    Added a development backend endpoint in.

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = ''; // Production server
+export const BASE_URL = 'http://localhost:8081/api/v1'; // Development server
 /* URLS */
 export const HOME_PAGE_URL = '/';
 
--- app/store/Urls.ts
+++ app/store/Urls.ts
@@ -1,1 +1,3 @@
-const AWS_API_BASE = '';
+const AWS_API_BASE = 'http://localhost:8081/api/v1';
+
+export const ADD_NEW_POST_API = `${AWS_API_BASE}/post`;
