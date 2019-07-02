de0b5ce9f18825dd7b44d8ea6f92a4c114614a38
Kevin Wang <rocwang0316@gmail.com>
2019-07-02 10:35:45 -0700

    Added a fab button and an AddPostDialog

--- app/components/containers/HomePageContainer.tsx
+++ app/components/containers/HomePageContainer.tsx
@@ -1,9 +1,11 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Paper } from '@material-ui/core';
+import { Paper, Fab, Tooltip } from '@material-ui/core';
+import { Add as AddIcon } from '@material-ui/icons';
 import I18n from '@kevinwang0316/i18n';
 
 import PostList from '../PostList';
+import AddPostDialog from '../AddPostDialog';
 
 const useStyles = makeStyles({
   rootPaper: {
@@ -11,13 +13,30 @@ const useStyles = makeStyles({
     width: '80%',
     padding: 15,
   },
+  fab: {
+    position: 'fixed',
+    bottom: 20,
+    right: 20,
+  },
 });
 
 const HomePage = () => {
   const classes = useStyles({});
+  const [isOpenAddPost, setIsOpenAddPost] = useState(false);
+  const addPostDialogCloseCallback = useCallback(() => setIsOpenAddPost((state: boolean) => !state), []);
+
   return (
     <Paper className={classes.rootPaper}>
       <PostList />
+      <Tooltip title={I18n.get('addPost')} placement="left-start">
+        <Fab size="medium" color="secondary" aria-label="Add" className={classes.fab} onClick={addPostDialogCloseCallback}>
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+      <AddPostDialog
+        isOpen={isOpenAddPost}
+        handleClose={addPostDialogCloseCallback}
+      />
     </Paper>
   );
 };
