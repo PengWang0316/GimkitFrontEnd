23b32501068552224863db75f4942fdeb4e8fd97
Kevin Wang <rocwang0316@gmail.com>
2019-07-02 16:41:51 -0700

    Added two new fetching apis

--- app/store/Urls.ts
+++ app/store/Urls.ts
@@ -1,3 +1,5 @@
 const AWS_API_BASE = 'http://localhost:8081/api/v1';
 
 export const ADD_NEW_POST_API = `${AWS_API_BASE}/post`;
+export const FETCH_POSTS_API = `${AWS_API_BASE}/post`;
+export const FETCH_POST_COUNT_API = `${AWS_API_BASE}/post/count`;
